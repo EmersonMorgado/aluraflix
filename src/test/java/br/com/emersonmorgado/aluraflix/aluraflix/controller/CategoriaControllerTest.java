63027e47b27925903c62a5ef32569d7858077eeb
Emerson Morgado <silvestrevan@gmail.com>
2021-08-07 21:50:13 -0300

    Ajuste nos testes de automatizados

--- src/test/java/br/com/emersonmorgado/aluraflix/aluraflix/controller/CategoriaControllerTest.java
+++ src/test/java/br/com/emersonmorgado/aluraflix/aluraflix/controller/CategoriaControllerTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@Profile("test")
 @AutoConfigureMockMvc
 class CategoriaControllerTest {
 
